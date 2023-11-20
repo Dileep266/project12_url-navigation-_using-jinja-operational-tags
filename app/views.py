535fc3ef8ee4fed43a4a22ca93f73b7d30ffe0a4
Dileep266 <somaladileep@gmail.com>
2023-11-20 22:46:10 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+def rohit(request):
+    return render(request,'rohit.html')
+def kohli(request):
+    return render(request,'kohli.html')
+def dhoni(request):
+    return render(request,'dhoni.html')
